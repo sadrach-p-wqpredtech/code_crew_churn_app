eb1b20610bdcf2a5a7cb1b863329d7f799b432b7
sadrach-p-wqpredtech <46689639+sadrach-p-wqpredtech@users.noreply.github.com>
2021-05-05 13:56:39 -0400

    Update churn-app.py

--- churn-app.py
+++ churn-app.py
@@ -22,8 +22,12 @@ pd.set_option('display.max_rows', None)
 
 st.write("""
 # Churn Prediction App
-This app predicts the probability of a customer churning, 
-where churn mean the customer does not make another purchase after a period of time. 
+Customer churn is defined as the loss of customers after a certain period of time. Companies are interested in targeting customers
+who are likely to churn. They can target these customers with special deals and promotions to influence them to stay with
+the company. 
+
+This app predicts the probability of a customer churning using Telco Cusstomer data. Here
+customer churn means the customer does not make another purchase after a period of time. 
 
 """)
 
