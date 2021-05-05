f7fe7a6c59ecff2ffca8b1bf09d6c4f8f5e6b854
sadrach-p-wqpredtech <46689639+sadrach-p-wqpredtech@users.noreply.github.com>
2021-05-05 13:50:39 -0400

    Update churn-app.py

--- churn-app.py
+++ churn-app.py
@@ -10,6 +10,9 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
+import base64
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 
 pd.set_option('display.max_columns', None)
